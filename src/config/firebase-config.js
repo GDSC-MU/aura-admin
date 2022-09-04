340533b53e336f27de68aee331254fbe8dbf2806
khaled atia <khalodatia@gmail.com>
2022-09-04 22:32:07 +0200

    configuration

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,9 +1,10 @@
 export default {
-    apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-    authDomain: "myaurapp.firebaseapp.com",
-    databaseURL: "https://myaurapp.firebaseio.com",
-    projectId: "myaurapp",
-    storageBucket: "myaurapp.appspot.com",
-    messagingSenderId: "419511997060",
-    appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+    apiKey: "AIzaSyC9Nv4BIBkr3KnMKzkSzfWF_KCLVZFdSFQ",
+    authDomain: "gdsc-mu.firebaseapp.com",
+    databaseURL: "https://gdsc-mu-default-rtdb.firebaseio.com",
+    projectId: "gdsc-mu",
+    storageBucket: "gdsc-mu.appspot.com",
+    messagingSenderId: "483182951770",
+    appId: "1:483182951770:web:aea769a5319789f19b0763",
+    measurementId: "G-KJ2KDVTRQ6"
 }
